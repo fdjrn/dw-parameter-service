8a5292dd084a452105e4ca81626e3d12da88d3b6
fadjrin <fadjrin.saeful.ramdan@gmail.com>
2023-06-26 14:05:10 +0700

    code cleaning VoucherRepository

--- internal/db/repository/voucher.go
+++ internal/db/repository/voucher.go
@@ -24,9 +24,9 @@ func NewVoucherRepository() VoucherRepository {
 	}
 }
 
-func (a *VoucherRepository) Create() (interface{}, error) {
+func (r *VoucherRepository) Create() (interface{}, error) {
 
-	result, err := db.Mongo.Collection.Voucher.InsertOne(context.TODO(), a.Model)
+	result, err := db.Mongo.Collection.Voucher.InsertOne(context.TODO(), r.Model)
 
 	if err != nil {
 		return nil, err
@@ -36,18 +36,18 @@ func (a *VoucherRepository) Create() (interface{}, error) {
 
 }
 
-func (a *VoucherRepository) SoftDelete() error {
+func (r *VoucherRepository) SoftDelete() error {
 
 	result, err := db.Mongo.Collection.Voucher.UpdateOne(
 		context.TODO(),
 		bson.D{
-			{"partnerId", a.Model.PartnerID},
-			{"code", a.Model.Code},
+			{"partnerId", r.Model.PartnerID},
+			{"code", r.Model.Code},
 		},
 		bson.D{
 			{"$set", bson.D{
-				{"updatedAt", a.Model.UpdatedAt},
-				{"deactivatedAt", a.Model.DeactivatedAt},
+				{"updatedAt", r.Model.UpdatedAt},
+				{"deactivatedAt", r.Model.DeactivatedAt},
 			}},
 		})
 
@@ -64,21 +64,10 @@ func (a *VoucherRepository) SoftDelete() error {
 
 }
 
-// TODO: 3. fetch all voucher
-
-/*
-FindAll
-return:
-
-	data interfaces{},
-	totalDocument int64,
-	totalPages int,
-	err error
-*/
-func (a *VoucherRepository) FindAll() (interface{}, int64, int64, error) {
+func (r *VoucherRepository) FindAll() (interface{}, int64, int64, error) {
 
 	filter := bson.D{}
-	switch a.Pagination.Status {
+	switch r.Pagination.Status {
 	case entity.VoucherStatusActive:
 		filter = bson.D{{"deactivatedAt", bson.D{{"$eq", primitive.Null{}}}}}
 	case entity.VoucherStatusDeactivated:
@@ -86,9 +75,9 @@ func (a *VoucherRepository) FindAll() (interface{}, int64, int64, error) {
 	default:
 	}
 
-	filter = append(filter, bson.D{{"partnerId", a.Pagination.PartnerID}}...)
+	filter = append(filter, bson.D{{"partnerId", r.Pagination.PartnerID}}...)
 
-	skipValue := (a.Pagination.Page - 1) * a.Pagination.Size
+	skipValue := (r.Pagination.Page - 1) * r.Pagination.Size
 
 	ctx, cancel := context.WithTimeout(context.TODO(), 1*time.Second)
 	defer cancel()
@@ -96,7 +85,7 @@ func (a *VoucherRepository) FindAll() (interface{}, int64, int64, error) {
 	cursor, err := db.Mongo.Collection.Voucher.Find(
 		ctx,
 		filter,
-		options.Find().SetSkip(skipValue).SetLimit(a.Pagination.Size),
+		options.Find().SetSkip(skipValue).SetLimit(r.Pagination.Size),
 	)
 
 	if err != nil {
@@ -113,12 +102,12 @@ func (a *VoucherRepository) FindAll() (interface{}, int64, int64, error) {
 		return nil, 0, 0, errors.New("empty results or last pages has been reached")
 	}
 
-	totalPages := math.Ceil(float64(totalDocs) / float64(a.Pagination.Size))
+	totalPages := math.Ceil(float64(totalDocs) / float64(r.Pagination.Size))
 	return &vouchers, totalDocs, int64(totalPages), nil
 
 }
 
-func (a *VoucherRepository) FindByID(id interface{}) (interface{}, error) {
+func (r *VoucherRepository) FindByID(id interface{}) (interface{}, error) {
 	result := new(entity.Voucher)
 
 	err := db.Mongo.Collection.Voucher.FindOne(
@@ -133,7 +122,7 @@ func (a *VoucherRepository) FindByID(id interface{}) (interface{}, error) {
 	return result, nil
 }
 
-func (a *VoucherRepository) FindByVoucherID(id interface{}) (interface{}, error) {
+func (r *VoucherRepository) FindByVoucherID(id interface{}) (interface{}, error) {
 	result := new(entity.Voucher)
 
 	err := db.Mongo.Collection.Voucher.FindOne(
@@ -148,14 +137,14 @@ func (a *VoucherRepository) FindByVoucherID(id interface{}) (interface{}, error)
 	return result, nil
 }
 
-func (a *VoucherRepository) FindByCode() (interface{}, error) {
+func (r *VoucherRepository) FindByCode() (interface{}, error) {
 	result := new(entity.Voucher)
 
 	err := db.Mongo.Collection.Voucher.FindOne(
 		context.TODO(),
 		bson.D{
-			{"partnerId", a.Model.PartnerID},
-			{"code", a.Model.Code},
+			{"partnerId", r.Model.PartnerID},
+			{"code", r.Model.Code},
 		}).Decode(&result)
 
 	if err != nil {
@@ -165,20 +154,20 @@ func (a *VoucherRepository) FindByCode() (interface{}, error) {
 	return result, nil
 }
 
-func (a *VoucherRepository) Update() error {
+func (r *VoucherRepository) Update() error {
 
 	result, err := db.Mongo.Collection.Voucher.UpdateOne(
 		context.TODO(),
 		bson.D{
-			{"partnerId", a.Model.PartnerID},
-			{"code", a.Model.Code},
+			{"partnerId", r.Model.PartnerID},
+			{"code", r.Model.Code},
 		},
 		bson.D{
 			{"$set", bson.D{
-				{"description", a.Model.Description},
-				{"amount", a.Model.Amount},
-				{"price", a.Model.Price},
-				{"updatedAt", a.Model.UpdatedAt},
+				{"description", r.Model.Description},
+				{"amount", r.Model.Amount},
+				{"price", r.Model.Price},
+				{"updatedAt", r.Model.UpdatedAt},
 			}},
 		})
 
@@ -194,477 +183,3 @@ func (a *VoucherRepository) Update() error {
 	return nil
 
 }
-
-//func (a *VoucherRepository) getDefaultFilter(account *entity.AccountBalance) bson.D {
-//	filter := bson.D{
-//		{"merchantId", account.MerchantID},
-//		{"partnerId", account.PartnerID},
-//	}
-//
-//	if account.TerminalID != "" {
-//		filter = append(filter, bson.D{{"terminalId", account.TerminalID}}...)
-//	}
-//
-//	if account.Type > 0 {
-//		filter = append(filter, bson.D{{"type", account.Type}}...)
-//	}
-//
-//	return filter
-//}
-
-//func (a *VoucherRepository) findByFilter(filter interface{}) (interface{}, error) {
-//	account := new(entity.AccountBalance)
-//
-//	// filter condition
-//	err := db.Mongo.Collection.Account.FindOne(context.TODO(), filter).Decode(&account)
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	return account, nil
-//}
-
-/*
-FindAll
-function args:
-
-	queryParams: string
-
-return:
-
-	code: int,
-	accounts: interface{},
-	length: int,
-	err: error
-*/
-//func (a *VoucherRepository) FindAll(queryParams string) (int, interface{}, int, error) {
-//	filter := bson.D{}
-//
-//	if queryParams != "" {
-//		filter = bson.D{{"active", false}}
-//		if queryParams == "true" {
-//			filter = bson.D{{"active", true}}
-//		}
-//	}
-//
-//	ctx, cancel := context.WithTimeout(context.TODO(), 500*time.Millisecond)
-//	defer cancel()
-//
-//	cursor, err := db.Mongo.Collection.Account.Find(ctx, filter,
-//		options.Find().SetProjection(bson.D{{"secretKey", 0}, {"lastBalance", 0}}),
-//	)
-//	if err != nil {
-//		return fiber.StatusInternalServerError, nil, 0, err
-//	}
-//
-//	var accounts []entity.AccountBalance
-//	if err = cursor.All(context.TODO(), &accounts); err != nil {
-//		return fiber.StatusInternalServerError, nil, 0, err
-//	}
-//
-//	return fiber.StatusOK, &accounts, len(accounts), nil
-//}
-
-/*
-FindAllPaginated
-function args:
-
-	*request.PaginatedAccountRequest
-
-return:
-
-	code int,
-	data interfaces{},
-	totalDocument int64,
-	totalPages int,
-	err error
-*/
-//func (a *VoucherRepository) FindAllPaginated(request *entity.PaginatedAccountRequest) (int, interface{}, int64, int64, error) {
-//	var filter interface{}
-//	switch request.Status {
-//	case AccountStatusActive:
-//		filter = bson.D{{"active", true}}
-//	case AccountStatusDeactivated:
-//		filter = bson.D{{"active", false}}
-//	default:
-//		filter = bson.D{}
-//	}
-//
-//	skipValue := (request.Page - 1) * request.Size
-//
-//	ctx, cancel := context.WithTimeout(context.TODO(), 500*time.Millisecond)
-//	defer cancel()
-//
-//	cursor, err := db.Mongo.Collection.Account.Find(
-//		ctx,
-//		filter,
-//		options.Find().
-//			SetProjection(bson.D{{"secretKey", 0}, {"lastBalance", 0}}).
-//			SetSkip(skipValue).
-//			SetLimit(request.Size),
-//	)
-//
-//	if err != nil {
-//		return fiber.StatusInternalServerError, nil, 0, 0, err
-//	}
-//
-//	totalDocs, _ := db.Mongo.Collection.Account.CountDocuments(ctx, filter)
-//	var accounts []entity.AccountBalance
-//	if err = cursor.All(context.TODO(), &accounts); err != nil {
-//		return fiber.StatusInternalServerError, nil, 0, 0, err
-//	}
-//
-//	if len(accounts) == 0 {
-//		return fiber.StatusInternalServerError, nil, 0, 0, errors.New("empty results or last pages has been reached")
-//	}
-//
-//	totalPages := math.Ceil(float64(totalDocs) / float64(request.Size))
-//	return fiber.StatusOK, &accounts, totalDocs, int64(totalPages), nil
-//}
-
-// FindByID : id args accept interface{} or primitive.ObjectID
-// make sure to convert it first
-//func (a *VoucherRepository) FindByID(id interface{}, active bool) (int, interface{}, error) {
-//	// filter condition
-//	filter := bson.D{{"_id", id}}
-//	if active {
-//		filter = append(filter, bson.D{{"active", true}}...)
-//	}
-//
-//	account, err := a.findByFilter(filter)
-//	if err != nil {
-//		return fiber.StatusInternalServerError, nil, err
-//	}
-//
-//	return fiber.StatusOK, account, nil
-//}
-
-//func (a *VoucherRepository) FindOne(account *entity.AccountBalance) (interface{}, error) {
-//
-//	result, err := a.findByFilter(GetDefaultAccountFilter(account))
-//
-//	if err != nil {
-//		if err == mongo.ErrNoDocuments {
-//			return nil, err
-//		}
-//		return nil, err
-//	}
-//
-//	return result, nil
-//}
-
-//func (a *VoucherRepository) FindByUniqueID(id string, active bool) (int, interface{}, error) {
-//
-//	filter := bson.D{{"uniqueId", id}}
-//	if active {
-//		filter = bson.D{{"uniqueId", id}, {"active", true}}
-//	}
-//
-//	account, err := a.findByFilter(filter)
-//
-//	if err != nil {
-//		if err == mongo.ErrNoDocuments {
-//			//return fiber.StatusNotFound, nil, errors.New("account not found or it has been unregistered")
-//			return fiber.StatusNotFound, nil, err
-//		}
-//		return fiber.StatusInternalServerError, nil, err
-//	}
-//
-//	return fiber.StatusOK, account, nil
-//}
-
-//func (a *VoucherRepository) FindByActiveStatus(id string, status bool) (int, interface{}, error) {
-//	//id, _ := primitive.ObjectIDFromHex(accountId)
-//
-//	account, err := a.findByFilter(bson.D{
-//		{Key: "uniqueId", Value: id},
-//		{Key: "active", Value: status},
-//	})
-//
-//	if err != nil {
-//		return fiber.StatusInternalServerError, nil, err
-//	}
-//
-//	return fiber.StatusOK, account, nil
-//}
-//
-//func (a *VoucherRepository) DeactivateAccount(u *entity.UnregisterAccount) (int, error) {
-//
-//	account := new(entity.AccountBalance)
-//	account.PartnerID = u.PartnerID
-//	account.MerchantID = u.MerchantID
-//	account.TerminalID = u.TerminalID
-//
-//	// update field
-//	update := bson.D{
-//		{"$set", bson.D{
-//			{"active", false},
-//			{"updatedAt", time.Now().UnixMilli()},
-//		}},
-//	}
-//
-//	result, err := db.Mongo.Collection.Account.UpdateOne(context.TODO(), GetDefaultAccountFilter(account), update)
-//	if err != nil {
-//		return fiber.StatusInternalServerError, err
-//	}
-//
-//	if result.ModifiedCount == 0 {
-//		return fiber.StatusBadRequest, errors.New(
-//			"update failed, cannot find account with current uniqueId")
-//	}
-//
-//	return fiber.StatusOK, nil
-//}
-//
-//func (a *VoucherRepository) DeactivateMerchant(u *entity.UnregisterAccount) (int, error) {
-//	//id, _ := primitive.ObjectIDFromHex(u.VoucherID)
-//
-//	// filter condition
-//	filter := bson.D{{"merchantId", u.MerchantID}, {"partnerId", u.PartnerID}}
-//
-//	// update field
-//	update := bson.D{
-//		{"$set", bson.D{
-//			{"active", false},
-//			{"updatedAt", time.Now().UnixMilli()},
-//		}},
-//	}
-//
-//	result, err := db.Mongo.Collection.Account.UpdateOne(context.TODO(), filter, update)
-//	if err != nil {
-//		return fiber.StatusInternalServerError, err
-//	}
-//
-//	if result.ModifiedCount == 0 {
-//		return fiber.StatusBadRequest, errors.New(
-//			"update failed, cannot find account with current uniqueId")
-//	}
-//
-//	return fiber.StatusOK, nil
-//}
-//
-//func (a *VoucherRepository) InsertDeactivatedAccount(account *entity.UnregisterAccount) (int, interface{}, error) {
-//
-//	result, err := db.Mongo.Collection.UnregisterAccount.InsertOne(context.TODO(), account)
-//
-//	if err != nil {
-//		return fiber.StatusInternalServerError, nil, err
-//	}
-//
-//	return fiber.StatusCreated, result.InsertedID, nil
-//
-//}
-//
-//func (a *VoucherRepository) RemoveDeactivatedAccount(acc *entity.UnregisterAccount) (int, error) {
-//	filter := bson.D{{"uniqueId", acc.VoucherID}}
-//	result, err := db.Mongo.Collection.UnregisterAccount.DeleteOne(context.TODO(), filter)
-//
-//	if err != nil {
-//		return fiber.StatusInternalServerError, err
-//	}
-//
-//	if result.DeletedCount > 0 {
-//		return fiber.StatusNoContent, nil
-//	}
-//
-//	return fiber.StatusInternalServerError, errors.New("remove deactivated account failed, no document found")
-//}
-
-// ----------------- MERCHANTS ----------------
-
-//func (a *VoucherRepository) FindAllMerchant(request *entity.PaginatedAccountRequest) (int, interface{}, int64, int64, error) {
-//	//var filter interface{}
-//
-//	filter := bson.D{}
-//	switch request.Status {
-//	case AccountStatusActive:
-//		filter = append(filter, bson.D{{"active", true}}...)
-//	case AccountStatusDeactivated:
-//		filter = append(filter, bson.D{{"active", false}}...)
-//	default:
-//	}
-//
-//	filter = append(filter, bson.D{{"type", 2}}...)
-//
-//	skipValue := (request.Page - 1) * request.Size
-//
-//	ctx, cancel := context.WithTimeout(context.TODO(), 500*time.Millisecond)
-//	defer cancel()
-//
-//	cursor, err := db.Mongo.Collection.Account.Find(
-//		ctx,
-//		filter,
-//		options.Find().
-//			SetProjection(bson.D{{
-//				"secretKey", 0},
-//			//{"lastBalance", 0},
-//			}).
-//			SetSkip(skipValue).
-//			SetLimit(request.Size),
-//	)
-//
-//	if err != nil {
-//		return fiber.StatusInternalServerError, nil, 0, 0, err
-//	}
-//
-//	totalDocs, _ := db.Mongo.Collection.Account.CountDocuments(ctx, filter)
-//	var accounts []entity.AccountBalance
-//	if err = cursor.All(context.TODO(), &accounts); err != nil {
-//		return fiber.StatusInternalServerError, nil, 0, 0, err
-//	}
-//
-//	if len(accounts) == 0 {
-//		return fiber.StatusInternalServerError, nil, 0, 0, errors.New("empty results or last pages has been reached")
-//	}
-//
-//	totalPages := math.Ceil(float64(totalDocs) / float64(request.Size))
-//	return fiber.StatusOK, &accounts, totalDocs, int64(totalPages), nil
-//}
-//
-//func (a *VoucherRepository) FindByMerchantID(ab *entity.AccountBalance) (int, interface{}, error) {
-//
-//	filter := bson.D{
-//		{"merchantId", ab.MerchantID},
-//		{"partnerId", ab.PartnerID},
-//		{"terminalId", ab.TerminalID},
-//		{"type", AccountTypeMerchant},
-//	}
-//
-//	account, err := a.findByFilter(filter)
-//
-//	if err != nil {
-//		if err == mongo.ErrNoDocuments {
-//			return fiber.StatusNotFound, nil, err
-//		}
-//		return fiber.StatusInternalServerError, nil, err
-//	}
-//
-//	return fiber.StatusOK, account, nil
-//}
-//
-//func (a *VoucherRepository) FindByMerchantStatus(ab *entity.AccountBalance, active bool) (int, interface{}, error) {
-//
-//	filter := bson.D{
-//		{"merchantId", ab.MerchantID},
-//		{"partnerId", ab.PartnerID},
-//	}
-//
-//	if active {
-//		filter = append(filter, bson.D{{"active", true}}...)
-//	}
-//
-//	account, err := a.findByFilter(filter)
-//
-//	if err != nil {
-//		if err == mongo.ErrNoDocuments {
-//			return fiber.StatusNotFound, nil, err
-//		}
-//		return fiber.StatusInternalServerError, nil, err
-//	}
-//
-//	return fiber.StatusOK, account, nil
-//}
-//
-///*
-//CountMerchantMember
-//out params:
-//
-//	totalDoc int
-//	documents interface{}
-//	err error
-//*/
-//func (a *VoucherRepository) CountMerchantMember(account entity.AccountBalance) (int64, error) {
-//	filter := bson.D{
-//		{"partnerId", account.PartnerID},
-//		{"merchantId", account.MerchantID},
-//		{"active", account.Active},
-//	}
-//
-//	ctx, cancel := context.WithTimeout(context.TODO(), 1*time.Second)
-//	defer cancel()
-//
-//	totalDocs, err := db.Mongo.Collection.Account.CountDocuments(ctx, filter)
-//	if err != nil {
-//		return 0, err
-//	}
-//
-//	return totalDocs, nil
-//}
-//
-//func (a *VoucherRepository) FindMemberByMerchant(account entity.AccountBalance) (int64, interface{}, error) {
-//	filter := bson.D{
-//		{"partnerId", account.PartnerID},
-//		{"merchantId", account.MerchantID},
-//		{"active", account.Active},
-//	}
-//
-//	ctx, cancel := context.WithTimeout(context.TODO(), 2*time.Second)
-//	defer cancel()
-//
-//	cursor, err := db.Mongo.Collection.Account.Find(ctx, filter)
-//
-//	if err != nil {
-//		return 0, nil, err
-//	}
-//
-//	var accounts []entity.AccountBalance
-//	if err = cursor.All(context.TODO(), &accounts); err != nil {
-//		return 0, nil, err
-//	}
-//
-//	totalDocs, _ := db.Mongo.Collection.Account.CountDocuments(ctx, filter)
-//
-//	if len(accounts) == 0 {
-//		return 0, nil, errors.New("empty results or last pages has been reached")
-//	}
-//
-//	return totalDocs, &accounts, nil
-//}
-//
-//func (a *VoucherRepository) FindMemberByMerchantPaginated(request *entity.PaginatedAccountRequest) (int, interface{}, int64, int64, error) {
-//	filter := bson.D{}
-//	switch request.Status {
-//	case AccountStatusActive:
-//		filter = bson.D{{"active", true}}
-//	case AccountStatusDeactivated:
-//		filter = bson.D{{"active", false}}
-//	default:
-//	}
-//
-//	filter = append(filter, bson.D{
-//		{"partnerId", request.PartnerID},
-//		{"merchantId", request.MerchantID},
-//		{"type", request.Type},
-//	}...)
-//
-//	skipValue := (request.Page - 1) * request.Size
-//
-//	ctx, cancel := context.WithTimeout(context.TODO(), 10*time.Second)
-//	defer cancel()
-//
-//	cursor, err := db.Mongo.Collection.Account.Find(
-//		ctx,
-//		filter,
-//		options.Find().
-//			SetProjection(bson.D{{"secretKey", 0}, {"lastBalance", 0}}).
-//			SetSkip(skipValue).
-//			SetLimit(request.Size),
-//	)
-//
-//	if err != nil {
-//		return fiber.StatusInternalServerError, nil, 0, 0, err
-//	}
-//
-//	totalDocs, _ := db.Mongo.Collection.Account.CountDocuments(ctx, filter)
-//	var accounts []entity.AccountBalance
-//	if err = cursor.All(context.TODO(), &accounts); err != nil {
-//		return fiber.StatusInternalServerError, nil, 0, 0, err
-//	}
-//
-//	if len(accounts) == 0 {
-//		return fiber.StatusInternalServerError, nil, 0, 0, errors.New("empty results or last pages has been reached")
-//	}
-//
-//	totalPages := math.Ceil(float64(totalDocs) / float64(request.Size))
-//	return fiber.StatusOK, &accounts, totalDocs, int64(totalPages), nil
-//}
